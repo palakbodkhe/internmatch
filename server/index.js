e5bd97e6108f8fd0fcb6a3e50b1f5286c0ce729f
palakbodkhe <palakbodkhe2617@gmail.com>
2026-04-07 07:57:42 +0530

    fix cors for all ports

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      process.env.CLIENT_URL || "http://localhost:5173",
+    ],
     credentials: true,
   }),
 );
